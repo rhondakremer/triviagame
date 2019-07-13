2ff21498da823ee142aa1970729a13f4bfe361aa
Rhonda Kremer <rhondakremer@gmail.com>
2019-07-12 20:49:59 -0400

    created a function that gets selected radio value and compares it with correct answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,24 @@
 $( document ).ready(function() {
     
     var question1 = {
+        name : 'question1',
         question : "How cute is Szilard?",
-        answera : "Super cute",
-        answerb : "Totally cute",
-        answerc : "The cutest",
-        answerd : "Puppy"
+        choices : ["Super cute", "Totally cute", "The cutest","Puppy"],
+        correct: "3"
     }
 
     var question2 = {
+        name : "question2",
         question : "this is number 2?",
-        answera : "possible solution",
-        answerb : "another answer",
-        answerc : "this one might be right",
-        answerd : "probably not"
+        choices : ["possible solution", "another answer", "this one might be right", "probably not"],
+        correct : "0",
     }
 
     function displayQA (each) {
-        $("#game").append("<br>" + each.question + "<br><input type='radio' name='question1' value='choice1'>" + each.answera + "<input type='radio' name='question1' value='choice1'>" + each.answerb + "<input type='radio' name='question1' value='choice1'>" + each.answerc + "<input type='radio' name='question1' value='choice1'>" + each.answerd + "<br>");
+        
+        $("#game").append("<br>" + each.question + "<br><input type='radio' name=" +each.name+ " value='0'>" + each.choices[0] + "<input type='radio' name=" + each.name + " value='1'>" + each.choices[1] + "<input type='radio' name=" + each.name + " value='2'>" + each.choices[2] + "<input type='radio' name=" +each.name+ " value='3'>" + each.choices[3] + "<br>");
     }
+
 
     var timeLeft = 10;
 
@@ -33,17 +33,40 @@ $( document ).ready(function() {
         } 
         
     };
+    var radioButton;
+    function compareAnswers (each) {
+        var radioButton = $('input[type=radio]:checked').val();
+        console.log(radioButton);
+        console.log(each.correct);
+        if (radioButton == each.correct) {
+            console.log("You don't suck");
+        } else if (radioButton !== each.correct) {
+            console.log("gay");
+        }
+    }
+
+   
     
     
-    $( "#start" ).click(function() {
+    $("#start").click(function() {
         $("#button").empty();
         displayQA(question1);
         displayQA(question2);
         countdown(1000);
+        $("#submitButton").show();
         
   
       });
     
+    $("#submit").click(function() {
+        console.log("hey bitch");
+        compareAnswers(question1);
+        compareAnswers(question2);
+    
+        
+        
+  
+      });
     
 
 // when start button is clicked, show hidden div and start timer
