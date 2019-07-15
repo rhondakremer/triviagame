6bb0e853b6cfae1d638d628efa965e1bdfa6b046
Rhonda Kremer <rhondakremer@gmail.com>
2019-07-15 17:04:58 -0400

    added some style, set up timeout when submit button is pressed, added submit actions to timeout

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,45 @@
 $(document).ready(function () {
 
     var questions = [
+
         {
-            question: "Which of these options is the earliest form of French horn?",
-            choices: ["Corno di caccia", "Trumpet", "Animal horns", "Hunting horn"],
-            correct: "Animal horns",
+            question: "Who was the founder of Chico, CA?",
+            choices: ["Charles H. Bennett", "General John Bidwell", "Arnold Schwarzenegger", "Ulysses Grant"],
+            correct: "General John Bidwell"
         }
         ,
         {
-            question: "Which horn player left his position as Principal Horn of a major symphony orchestra to become horn professor at Indiana University saying, 'I would rather quit several years too soon than 10 minutes too late'?",
-            choices: ["Phillip Farkas", "Dale Clevenger", "Myron Bloom", "Dennis Brain"],
-            correct: "Phillip Farkas"
-        }]
+            question: "In what year was the city of Chico founded?",
+            choices: ["1801", "1840", "1860", "1872"],
+            correct: "1860",
+        }
+        ,
+        {
+            question: "Who were the original inhabitants of the land that became Chico?",
+            choices: ["Mechoopda Maidu", "Wamapoke", "Apache", "Cherokee"],
+            correct: "Mechoopda Maidu"
+        },
+        {
+            question: "Chico is home to the world's largest _____",
+            choices: ["Butter churn", "Gold nugget", "Yo-yo", "Slinky"],
+            correct: "Yo-yo",
+        }
+        ,
+        {
+            question: "Which brewery is located in Chico, CA?",
+            choices: ["Mission Brewery", "Coachella Valley Brewing Company", "El Segundo Brewing Company", "Sierra Nevada Brewing Company"],
+            correct: "Sierra Nevada Brewing Company",
+        }
+        ,
+        {
+            question: "Bidwell Park is the _____ largest municipal park in CA.",
+            choices: ["1st", "2nd", "3rd", "4th"],
+            correct: "3rd",
+        }
+    ]
 
 
-    var start = function () {
+    function start() {
         for (let i = 0; i < questions.length; i++) {
             $('#game').append("<h2>" + questions[i].question + "</h2>");
             for (let j = 0; j < questions[i].choices.length; j++) {
@@ -23,16 +48,31 @@ $(document).ready(function () {
         }
     }
 
+    function displayCorrect() {
+        for (let i = 0; i < questions.length; i++) {
+            $('#game').append("<h2>" + questions[i].question + "</h2>");
+            for (let j = 0; j < questions[i].correct.length; j++) {
+                $('#game').append(questions[i].correct[j]);
+            }
+        }
+    }
+
 
 
 
     var timeLeft = 10;
-
+    var timer;
 
     function countdown() {
-        setTimeout(countdown, 1000);
+        timer = setTimeout(countdown, 1000);
         if (timeLeft == -1) {
-            clearTimeout;
+            clearTimeout(timer);
+            $("#timer").text("You ran out of time!");
+            compareAnswers();
+            $("#game").empty();
+            $("#submitButton").empty();
+            $("#results").html("Correct: " + game.correct + "<br>Incorrect: " + game.incorrect);
+            displayCorrect();
         } else {
             $("#timer").html("You have " + timeLeft + " seconds left!");
             timeLeft--;
@@ -49,44 +89,21 @@ $(document).ready(function () {
     //var radioButton = [card.children("input:checked").val()];
     //var radioButton = $('input[type=radio]:checked').val();
 
-    function compareAnswers() { 
+    function compareAnswers() {
         for (let i = 0; i < questions.length; i++) {
-        if ($("input[name='question" +i+ "']:checked").val() === questions[i].correct) {
-
-            console.log("yes")
-        } else {
-            console.log("no");
-        };
-
-
-
-
-
-
-    
-        
-        /*var correctAnswer;
-        var radioButton = $("#game").children("input:checked").val();
-
-
-        for (let i = 0; i < questions.length; i++) {
-            correctAnswer = questions[i].correct;
-            if (radioButton[i] === correctAnswer) {
-                console.log(radioButton);
+            if ($("input[name='question" + i + "']:checked").val() === questions[i].correct) {
                 game.correct++;
-                console.log("You rock! ")
+                console.log("yes")
             } else {
                 game.incorrect++;
-                console.log("Make better life choices!" + radioButton)
-            }
-         
-        } */
-};
+                console.log("no");
+            };
 
-};
+        };
+
+    };
 
     $("#start").click(function () {
-        console.log("I am being clicked!");
         $("#button").empty();
         start();
         countdown(1000);
@@ -94,13 +111,16 @@ $(document).ready(function () {
     });
 
     $("#submit").click(function () {
-        console.log("I work");
         compareAnswers();
-        
-        
+        $("#game").empty();
+        $("#timer").empty();
+        clearTimeout(timer)
+        $("#submitButton").empty();
+        $("#results").html("Correct: " + game.correct + "<br>Incorrect: " + game.incorrect);
+        displayCorrect();
     });
 
-    
+
 
 
 
